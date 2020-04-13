81576ee1958d5217973f9ed9569acc4b641d0f52
fannyhasbi <fnyhsbi@gmail.com>
2020-04-13 20:49:58 +0700

    copy to clipboard

--- emocrypt/main.js
+++ emocrypt/main.js
@@ -1,16 +1,36 @@
-function encrypt(){
-  Swal.fire({
-    text: 'Something went wrong!',
-  });
+$(document).ready(function(){
+  new ClipboardJS('#copy-btn');
+});
 
-  return;
+function validate(){
   let message = document.getElementById('message').value;
   let key = document.getElementById('key').value;
 
-  let emo = new EmoCrypt.EmoCrypt();
-  cipher = emo.encrypt(message, key);
-  
-  console.log('hasilnya :', cipher);
+  return message.length > 0 && key.length > 0;
+}
 
-  document.getElementById('result').innerHTML = cipher;
+function encrypt(){
+  let message = document.getElementById('message').value;
+  let key = document.getElementById('key').value;
+
+  if(validate()){
+    // encryption
+    let emo = new EmoCrypt.EmoCrypt();
+    cipher = emo.encrypt(message, key);
+
+    // content
+    let copyBtn = `<button id="copy-btn" class="btn btn-primary" data-clipboard-target="#encryption-result">Copy</button>`;
+    let cipherTextArea = `<textarea id="encryption-result" class="w-100 h-100" readonly>${cipher}</textarea>`;
+    let html = $('<div>')
+      .append(cipherTextArea)
+      .append(copyBtn);
+
+    // show the cipher
+    Swal.fire({
+      icon: 'success',
+      html: html,
+      showCloseButton: true,
+      showConfirmButton: false,
+    });
+  }
 }
