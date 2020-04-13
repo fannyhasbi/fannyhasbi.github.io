3184e4d41203c1c22ccdf3a0c4d8cf7cf73ac099
fannyhasbi <fnyhsbi@gmail.com>
2020-04-13 22:42:45 +0700

    decrypting process

--- emocrypt/main.js
+++ emocrypt/main.js
@@ -13,24 +13,49 @@ function encrypt(){
   let message = document.getElementById('message').value;
   let key = document.getElementById('key').value;
 
-  if(validate()){
-    // encryption
-    let emo = new EmoCrypt.EmoCrypt();
-    cipher = emo.encrypt(message, key);
+  if(!validate()) return
+  
+  // encryption
+  let emo = new EmoCrypt.EmoCrypt();
+  cipher = emo.encrypt(message, key);
 
-    // content
-    let copyBtn = `<button id="copy-btn" class="btn btn-primary" data-clipboard-target="#encryption-result">Copy</button>`;
-    let cipherTextArea = `<textarea id="encryption-result" class="w-100 h-100" readonly>${cipher}</textarea>`;
-    let html = $('<div>')
-      .append(cipherTextArea)
-      .append(copyBtn);
+  // content
+  let copyBtn = `<button id="copy-btn" class="btn btn-primary" data-clipboard-target="#encryption-result">Copy</button>`;
+  let cipherTextArea = `<textarea id="encryption-result" class="w-100 h-100" readonly>${cipher}</textarea>`;
+  let html = $('<div>')
+    .append(cipherTextArea)
+    .append(copyBtn);
 
-    // show the cipher
+  // show the cipher
+  Swal.fire({
+    icon: 'success',
+    html: html,
+    showCloseButton: true,
+    showConfirmButton: false,
+  });
+}
+
+function decrypt(){
+  let cipher = document.getElementById('message').value;
+  let key = document.getElementById('key').value;
+
+  if(!validate()) return
+
+  // decryption
+  let emo = new EmoCrypt.EmoCrypt();
+  message = emo.decrypt(cipher, key);
+
+  if(message.length > 0){
     Swal.fire({
       icon: 'success',
-      html: html,
+      text: message,
       showCloseButton: true,
-      showConfirmButton: false,
+    });
+  } else {
+    Swal.fire({
+      icon: 'error',
+      text: 'Cipher or secret key wrong',
+      showCloseButton: true,
     });
   }
 }
